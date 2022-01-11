84976136936eb3d1033cea22580c8bb8e032bda9
Yulia Lepner <ylepner@gmail.com>
2022-01-11 22:23:18 +0100

    feat: implement getIdentityMatrix function

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -466,8 +466,13 @@ function sortCitiesArray(arr) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  const newArr = new Array(n).fill(null);
+  return newArr.map((_, i) => {
+    const arr = new Array(n).fill(0);
+    arr[i] = 1;
+    return arr;
+  });
 }
 
 /**
